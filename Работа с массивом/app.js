63dd7d345810b19c7caea57f817bf5b539c03142
IGOR EVSSEV <andrey.semin.evseev@gmail.com>
2023-10-20 22:02:22 +0300

    Попытки на 50% закончились успехом, но нужно тестить рекурсию (20.23)

--- Работа с массивом/app.js
+++ Работа с массивом/app.js
@@ -11,25 +11,47 @@ var inorderTraversal = function*(arr) {
 
         for (let index = 0; index < arr.length; index++) {
             if (typeof arr[index] == 'object') {
-                console.log(123);
+                // создаем элемент, по которому в дальнейшем будем удалять из массива
+                obj = arr[index]
+                
+                // перебор элементов внутри массива
+                // for (let index2 = 0; index2 < arr[index].length; index2+=1) {
+                //     console.log(arr[index], 'Это внутренний массив')
+
+                //     // вставка элемента под. массива в главный массив
+                //     arr.splice(index, 0, arr[index][index2]);
+                //     console.log(`вставили элемент ${arr[index]}`);
+
+                //     console.log(arr);
+                // }
+
+
+                // Второй вариант
+                for (const iterator of arr[index]) {
+
+                    // вставка элемента под. массива в главный массив
+                    // Можно суммировать с итератором, но пока не вышло
+                    arr.splice(index + arr.indexOf(iterator), 0, iterator);
+                    console.log(`"Элемент ${iterator} вствлен на место ${index + arr.indexOf(iterator)}. Итератор номер - ${arr.indexOf(iterator)}`);
+
+                    console.log(arr);
+                }
+
+                // Удаление разбитого элемента
+                arr.splice(arr.indexOf(obj), 1)
             }
             
         }
 
 
 
-
-
-
-
-
         
-        if (n == 50) {
+        if (n == 5) {
             break
         }
+
+        console.log(arr);
     }
-
-
 
     console.log(arr);
     for (let index = 0; index < arr.length; index++) {
@@ -37,7 +59,16 @@ var inorderTraversal = function*(arr) {
     }
 };
 
-const gen = inorderTraversal([[[6]],[1,3],[]]);
 
+// ко мне тольько сейчас пришла идей испольщовать рекурсию
+
+mass = [[[6]],[1,3],[]]
+// mass = [4234,63453,123,[34545,867],[[0]],[[]]]
+
+const gen = inorderTraversal(mass);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
 console.log(gen.next().value);
 console.log(gen.next().value);
